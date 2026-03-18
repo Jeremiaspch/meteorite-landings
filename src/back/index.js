9da76da4942bf2eaa14a2800730906e92735f7f5
Jeremías <jrmspls@gmail.com>
2026-03-18 21:08:30 +0100

    only json

--- src/back/index.js
+++ src/back/index.js
@@ -6,6 +6,18 @@ const satellitesAPI = require("./api/active-satellites.js");
 function loadBackend(app) {
     const BASE_URL_API = "/api/v1";
 
+    // (Fuerza recibir solo JSON) ---
+    app.use(BASE_URL_API, (req, res, next) => {
+        if (req.method === "POST" || req.method === "PUT") {
+            // Comprueba si tiene cuerpo y si NO es JSON
+            if (req.get("Content-Type") && !req.is("application/json")) {
+                //Devolvemos un JSON
+                return res.status(415).json({ error: "Unsupported Media Type: Only JSON allowed" });
+            }
+        }
+        next();
+    });
+
     // --- Enchufamos las APIs ---
     app.use(BASE_URL_API + "/space-launches", spaceLaunchesAPI);
     app.use(BASE_URL_API + "/meteorite-landings", meteoriteLandingsAPI);
