5d57294bda4285baf391be04717c52898997e144
Jeremías <jrmspls@gmail.com>
2026-03-27 11:29:26 +0100

    playwright añadido

---
+++ tests/e2e/routes.spec.js
@@ -0,0 +1,13 @@
+// @ts-check
+/*
+import { test, expect } from '@playwright/test';
+
+let app = 'http://localhost:10000'
+test('has title', async ({ page }) => {
+  await page.goto('app');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Proyecto SOS2526-14/);
+});
+
+*/
